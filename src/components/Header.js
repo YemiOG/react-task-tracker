feaad16d8250e1b337d402266eb5876cda8f96d5
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2021-12-22 20:31:39 +0100

    A simple Task planner

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+   return ( 
+    <header className='header'>
+        <h1>
+        {title}</h1>
+        <Button color = 'green' text ='Add'></Button>
+       
+    </header>
+   )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
